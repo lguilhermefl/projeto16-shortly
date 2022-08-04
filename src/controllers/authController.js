b472ff1d6be0cebd3413c4e5eb97c98aad13afd9
luiz <lguilhermefl@gmail.com>
2022-08-04 11:41:34 -0300

    feat: signin controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -20,8 +20,36 @@ async function signUp(req, res) {
 
     await connection.query(`
         insert into users (name, email, password)
-        values ($1, $2, $3)
+        values ($1, $2, $3);
     `, [name, email, passwordHash]);
 
     res.sendStatus(201);
 };
+
+async function signIn(req, res) {
+    const { user } = res.locals;
+    const { email, password } = user;
+
+    const { rowCount: userRegistered } = await connection.query(`
+        select *
+        from users
+        where email=$1;
+    `, [email]);
+
+    const { id, passwordHash } = userRegistered;
+
+    if (userRegistered !== 0 && bcrypt.compareSync(password, passwordHash)) {
+        const token = uuid();
+
+        await connection.query(`
+            insert into sessions (user_id, token)
+            values ($1, $2);
+        `, [id, token]);
+
+        res.status(200).send({ token });
+    } else {
+        res.sendStatus(401);
+    };
+};
+
+export { signUp, signIn };
