015d19d524cb059d779e826ac52220ef3678c7c7
luiz <lguilhermefl@gmail.com>
2022-08-05 16:28:09 -0300

    chorus: add auth controllers to auth router

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { signUp, signIn } from '../controllers/authController.js';
 
 const authRouter = Router();
 
-authRouter.post('/signup');
-authRouter.post('/signin');
+authRouter.post('/signup', signUp);
+authRouter.post('/signin', signIn);
 
 export default authRouter;
